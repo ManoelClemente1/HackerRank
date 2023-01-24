407541dda6bd5aa5e9c09489d64a8e7478621e08
Manoel C <manoelclemente9@outlook.com>
2023-01-24 19:15:56 -0300

    add AveragePrice between two products

---
+++ src/POO/AveragePrice.java
@@ -0,0 +1,37 @@
+package POO;
+
+import java.util.List;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class AveragePrice {
+
+    public static void main(String[] args) {
+
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        Product[] vect = new Product[n];
+
+        for(int i = 0; i < n; i++){
+            sc.nextLine();
+            String name = sc.nextLine();
+            double price = sc.nextDouble();
+            Product product = new Product(name, price);
+            vect[i] = product;
+        }
+
+        double sum = 0.0;
+        for(int i = 0; i < n; i++){
+            sum += vect[i].getprice();
+        }
+
+        double average = sum / n;
+
+        System.out.println("AVERAGE BETWEEN " + n + " PRODUCTS ARE = " + average);
+
+        sc.close();
+
+    }
+}
